d6926db6b8f6abd1e1fd0ceff7eea2a4acc3a1ed
Kirill Surikow <K.Surikow94@gmx.de>
2023-03-01 17:42:36 +0100

    service

--- src/app/restaurant-overview/restaurant-overview.component.ts
+++ src/app/restaurant-overview/restaurant-overview.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Obj } from '@popperjs/core';
 import { Restaurants } from 'src/models/restaurants.class';
 import { FilterService } from './../services/filter.service';
 
